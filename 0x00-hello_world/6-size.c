fedbc984f184178838230577e90b072056f01476
LilyMwendwa <mwendwalily2@gmail.com>
2022-09-10 17:36:18 -0700

    sizes of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-is the entry point for the program
+ *
+ * Return:zero if no error, non zero value if errors
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+   printf("Size of char: %ld byte(s)\n", sizeof(c));
+   printf("Size of int: %ld byte(s)\n", sizeof(i));
+   printf("Size of long int: %ld byte(s)\n", sizeof(li));
+   printf("Size of long long int: %ld byte(s)\n", sizeof(lli));
+   printf("Size of float: %ld byte(s)\n", sizeof(f));
+   return (0);
+}
