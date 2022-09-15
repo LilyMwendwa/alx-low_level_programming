ebb65f83cb198d1d1b68c65dd605b11c28aa03e7
LilyMwendwa <mwendwalily2@gmail.com>
2022-09-15 07:45:38 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- allows us to print either number
+ * or fizz or buzz or fizzbuzz
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+	if ((num % 3 == 0) && (num % 5 == 0))
+	{
+		printf("FizzBuzz ");
+	}
+	else if ((num % 3) == 0)
+	{
+		printf("Fizz ");
+	}
+	else if ((num % 5) == 0)
+	{
+		if (num != 100)
+		{
+		printf("Buzz ");
+		}
+		else
+		{
+		printf("Buzz ");
+		}
+	}
+	}
+}
