7908fc054f48bde846f9980185bbcb6862ef6c03
LilyMwendwa <mwendwalily2@gmail.com>
2022-09-19 13:06:26 -0700

    reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,6 +13,26 @@ void rev_string(char *s)
 
 	for (i = 0; i < len / 2; i++)
 	{
-	tmp = *(s + i);	
+	tmp = *(s + i);
+	*(s + i) = *(s + len - i - 1);
+	*(s + len - i - 1) = tmp;
 	}
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length of the given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+	len++;
+	}
+	return (len);
 }
