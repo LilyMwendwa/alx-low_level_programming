d1be3666493dcaa6236a1254ef35c77296fd8533
LilyMwendwa <mwendwalily2@gmail.com>
2022-10-15 16:09:21 -0700

    Function Pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 
 /**
+ * print_opcodes- print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ * Return: void
+ */
+
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
  * main- prints its own opcodes
  * @argc: number of arguments
  * @argv: array of arguments
@@ -10,32 +30,19 @@
 
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	int n;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[i]);
-
-	if (bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
-	}
+	print_opcodes((char *)&main, n);
 	return (0);
 }
